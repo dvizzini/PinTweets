d9b7a19d0fc5dcf34e9bf93d5e9ff4c5f68c2743
Daniel Vizzini <dvizzini@gmail.com>
2012-10-25 12:11:35 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinTweets::Application.config.secret_token = '55c1a9afe5e8f1dea65e2db0a8e273aac66e85c4e9cb1e84259d64b29012241a7829d4bf58689e6063f2d7a96731863645caf5ef69a1b855adfdab2616b10def'
